73e908094281ac8c6b1f8f0a30432abe87d48081
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-11-07 11:12:27 +0900

    2022年11月07日朝分

---
+++ src/chapter03/ProductTest.java
@@ -0,0 +1,8 @@
+package chapter03;
+
+public class ProductTest {
+    public static void main(String[] args) {
+        Product p = new Product("A200", "XPS8950", 244800);
+        System.out.println(p);
+    }
+}
