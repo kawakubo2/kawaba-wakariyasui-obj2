89f165503ec19cc86dbe17d868dbe00688012c2f
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-11-07 22:39:05 +0900

    2022年11月07日夜分

--- src/chapter03/Product.java
+++ src/chapter03/Product.java
@@ -1,6 +1,13 @@
 package chapter03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 public class Product {
     private String number;
@@ -23,7 +30,26 @@ public class Product {
         this.date = LocalDate.now();
         this.stock = true;
     }
-    public Product(String fileName) {
+    public Product(String fileName) throws IOException, IllegalArgumentException {
+        Path path = Paths.get(fileName);
+        try (BufferedReader in = Files.newBufferedReader(path);) {
+            String temp = in.readLine();
+            String[] strArray = temp.split(",");
+            if (strArray.length != 5) {
+                throw new IllegalArgumentException("ファイル形式が異なります。");
+            }
+            this.number = strArray[0];
+            this.name = strArray[1];
+            this.price = Integer.parseInt(strArray[2]);
+            List<Integer> intArray = Arrays.stream(strArray[3].split("-"))
+                                            .map(Integer::parseInt) 
+                                            .toList();
+            this.date = LocalDate.of(intArray.get(0), intArray.get(1), intArray.get(2));
+            this.stock = "1".equals(strArray[4]) ? true : false;
+        } catch (IOException | IllegalArgumentException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
     public Product() {}
     public String getNumber() {
--- src/chapter03/ProductTest.java
+++ src/chapter03/ProductTest.java
@@ -1,8 +1,16 @@
 package chapter03;
 
+import java.io.IOException;
+
 public class ProductTest {
     public static void main(String[] args) {
-        Product p = new Product("A200", "XPS8950", 244800);
-        System.out.println(p);
+        Product p1 = new Product("A200", "XPS8950", 244800);
+        System.out.println(p1);
+        try {
+            Product p2 = new Product("product.csv");
+            System.out.println(p2);
+        } catch (IOException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 }
