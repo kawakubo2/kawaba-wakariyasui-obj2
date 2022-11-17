f3235a3e75243ac85cf05906dc28d4021e15b485
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-11-18 08:16:11 +0900

    2022年11月18日朝分

---
+++ src/chapter09/Foo.java
@@ -0,0 +1,11 @@
+package chapter09;
+
+public class Foo {
+  private double x = 10;
+  public double calc() {
+    return x;
+  }
+  public double getX() {
+    return x;
+  }
+}
---
+++ src/chapter09/Review5.java
@@ -0,0 +1,12 @@
+package chapter09;
+
+public class Review5 {
+  public static void main(String[] args) {
+    Foo foo1 = new Foo();
+    System.out.println(foo1.calc());
+    Foo foo2 = new Bar();
+    System.out.println(foo2.calc());
+    Foo foo3 = new Baz();
+    System.out.println(foo3.calc());
+  }
+}
