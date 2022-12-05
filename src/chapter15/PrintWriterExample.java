13d1140bb40056bb966662dd038799d92ebf41fc
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-12-05 09:30:37 +0900

    2022年12月05日朝分

---
+++ src/chapter15/PrintWriterExample.java
@@ -0,0 +1,18 @@
+package chapter15;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrintWriterExample {
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter("data.txt", "UTF-8");){
+            out.print(100);
+            out.print("\t");
+            out.print("田中宏");
+            out.print("\t");
+            out.println(60.5);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
