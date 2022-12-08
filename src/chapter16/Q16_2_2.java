08f27cd3606dafbea479f8adb6d9f20c7adc40d0
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-12-08 23:15:43 +0900

    2022年12月08日昼分

---
+++ src/chapter16/Q16_2_2.java
@@ -0,0 +1,19 @@
+package chapter16;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+record Order(String code, int quantity, LocalDate date) {}
+
+public class Q16_2_2 {
+    public static void main(String[] args) {
+        var o1 = new Order("A-202", 3, LocalDate.of(2025, 3,1));
+        var o2 = new Order("A-203", 5, LocalDate.of(2025, 3, 4));
+        var list = new ArrayList<Order>();
+        list.add(o1);
+        list.add(o2);
+        for (Order o: list) {
+            System.out.println(o);
+        }
+    }
+}
