dec86edf6684e22eeaa69c6c62729fdb2d00cdd0
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-12-10 23:07:29 +0900

    2022年12月10日夜分2

---
+++ src/chapter17/Review3And4.java
@@ -0,0 +1,18 @@
+package chapter17;
+
+import java.util.Map;
+
+public class Review3And4 {
+    public static void main(String[] args) {
+        Map<String, String> fruitMap = Map.of("banana", "バナナ", "cherry", "チェリー", "apple", "リンゴ", "pear", "ナシ", "grape", "ブドウ");
+        for (Map.Entry<String, String> entry: fruitMap.entrySet()) {
+            System.out.printf("%-15s - %s%n", entry.getKey(), entry.getValue());
+        }
+
+        String name1 = "banana";
+        String name2 = "pineapple";
+
+        System.out.println(name1 + ": " + fruitMap.get(name1));
+        System.out.println(name2 + ": " + fruitMap.getOrDefault(name2, "フルーツ"));
+    }
+}
