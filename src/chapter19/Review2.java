0a6e002d1e6752aa5d83df09f33b6e47ca987e8b
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-12-14 12:21:10 +0900

    2022年12月14日朝分

--- src/chapter19/Review2.java
+++ src/chapter19/Review2.java
@@ -3,6 +3,7 @@ package chapter19;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
@@ -17,16 +18,21 @@ public class Review2 {
         System.out.println(sup1.get());
 
         // (3)
+        Predicate<Book> pred1 = book1 -> book1.price() > 2000;
+        Book b1 = new Book(123457, "わかりやすいJavaオブジェクト指向編", 2800);
+        System.out.println(pred1.test(b1));
+
+        // (4)
         Function<Student, Integer> func1 = student1 -> student1.name().length();
         Student s1 = new Student(102, "田中一郎");
         System.out.println(func1.apply(s1));
 
-        // (4)
+        // (5)
         UnaryOperator<Double> una1 = d1 -> Math.pow(d1, 5);
         double d1 = 2.51188643150958;
         System.out.println(una1.apply(d1));
 
-        // (5)
+        // (6)
         BiConsumer<String, Integer> bic1 = (str1, i) -> System.out.println(str1.charAt(i));
         bic1.accept("Java", 2);
 
